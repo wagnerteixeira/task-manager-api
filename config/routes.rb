dba477f9f5b3ff00d03adf889cfc205d9fe50587
Wagner Bernardes Teixeira <Wagner@DESKTOP-D8G4ILP>
2018-03-28 00:57:13 -0300

    Configuração de rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
+require 'api_version_constraints'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: "/" do
-    
+    namespace :v1, path: "/", constraints: ApiVersionConstraint.new(version: 1, default: true) do
+    end
   end
 end
