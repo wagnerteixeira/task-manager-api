04dd5fe694dfcddc4b6d8b1bbf350b576c21bc75
Wagner Bernardes Teixeira <Wagner@DESKTOP-D8G4ILP>
2018-04-07 01:18:57 -0300

    Ajsutes pra funcionar a api web e ajustes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::UsersController < ApplicationController
+
     respond_to :json
+    
     def show
         begin
             @user = User.find(params[:id])
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,10 +7,10 @@ RSpec.describe User, type: :model do
   it { expect(user).to validate_presence_of(:email) }
   it { expect(user).to validate_uniqueness_of(:email).case_insensitive }
   it { expect(user).to validate_confirmation_of(:password) }
-  it { expect(user).to allow_value("wagner@email.com").for(:email) }
+  it { expect(user).to allow_value('wagner@email.com').for(:email) }
 
   # context 'when name is blank' do
-  #   before { user.name = " " }
+  #   before { user.name = ' ' }
   #   it { expect(user).not_to be_valid }
 
   # end
