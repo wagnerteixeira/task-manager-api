040abcf21ff73a2f0772d387c1d4bc05513e34a2
Wagner Bernardes Teixeira <Wagner@DESKTOP-D8G4ILP>
2018-04-07 01:54:15 -0300

    Aula 26 seção 4

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -30,4 +30,36 @@ RSpec.describe 'Users API', type: :request do
             end
         end
     end
+
+    describe 'POST /users' do 
+        before do
+            headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
+            post '/users', params: { user: user_params }, headers: headers
+        end
+        context 'when the request are valid' do 
+            let(:user_params){ attributes_for(:user) }
+
+            it 'returns status 201' do
+                expect(response).to have_http_status(201)
+            end
+
+            it 'returns json data for the created user' do
+                user_response = JSON.parse(response.body)    
+                expect(user_response['email']).to eq(user_params[:email])
+            end
+        end
+
+        context 'when the request are invalid' do 
+            let(:user_params){ attributes_for(:user, email: 'invalid_email@') }
+
+            it 'returns status 422' do
+                expect(response).to have_http_status(422)
+            end
+
+            it 'returns json data for the errors' do
+                user_response = JSON.parse(response.body)    
+                expect(user_response).to have_key('errors')
+            end
+        end
+    end
 end
